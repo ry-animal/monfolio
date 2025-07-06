15107c5cc83336ff6450bbb5ed0a5cd5f7129606
ryanimal <ryanlvv@gmail.com>
2025-07-06 16:10:20 -0700

    initial commit

---
+++ apps/server/src/index.ts
@@ -0,0 +1,32 @@
+import { env } from "cloudflare:workers";
+import { trpcServer } from "@hono/trpc-server";
+import { createContext } from "./lib/context";
+import { appRouter } from "./routers/index";
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+import { logger } from "hono/logger";
+
+const app = new Hono();
+
+app.use(logger());
+app.use("/*", cors({
+  origin: env.CORS_ORIGIN || "",
+  allowMethods: ["GET", "POST", "OPTIONS"],
+}));
+
+
+
+app.use("/trpc/*", trpcServer({
+  router: appRouter,
+  createContext: (_opts, context) => {
+    return createContext({ context });
+  },
+}));
+
+
+
+app.get("/", (c) => {
+  return c.text("OK");
+});
+
+export default app;
